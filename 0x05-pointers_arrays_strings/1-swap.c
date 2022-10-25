0feefee454c720a102f871805c58b0cf0817d617
SmartBrain35 <ramsonrutherford@gmail.com>
2022-10-25 13:05:44 +0000

    swapping task done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Takes a two int
+ * variables and swap thier values
+ * @a: first argumenet in swap_int
+ * @b: second aegument in swap_int
+ */
+
+void swap_int(int *a, int *b)
+{
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
