64a05cb78990b5e4f682298f40a3edae7a4b1071
SmartBrain35 <ramsonrutherford@gmail.com>
2022-10-25 13:10:19 +0000

    swap fixed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * swap_int - Takes a two int
- * variables and swap thier values
- * @a: first argumenet in swap_int
- * @b: second aegument in swap_int
- */
+* swap_int - Takes a two int
+* variables and swap thier values
+* @a: first argumenet in swap_int
+* @b: second aegument in swap_int
+*/
 
 void swap_int(int *a, int *b)
 {
