8ef518873cdfe476975f4afa8b5750455dfd65ba
SmartBrain35 <ramsonrutherford@gmail.com>
2022-10-25 21:09:25 +0000

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - takes a pointer to char variable and return its length
+ * @s: the pointer to a char
+ */
+
+int _strlen(char *s)
+{
+  if(*s == "\0")
+  {
+    return 0;
+  }else
+  {
+     return (1 + _strlen(++s));
+  }
+}
