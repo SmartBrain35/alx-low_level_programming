620ca3285930d030070e6ee375f0ad749e11b462
SmartBrain35 <ramsonrutherford@gmail.com>
2022-10-25 21:51:01 +0000

    hot fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strlen - takes a pointer to char variable and return its length
- * @s: the pointer to a char
- */
+* _strlen - takes a pointer to char variable and return its length
+* @s: the pointer to a char
+*/
 
 int _strlen(char *s)
 {
 if (*s == '\0')
 {
- return (0);
+return (0);
 } else
 {
- return (1 + _strlen(++s));
+return (1 + _strlen(++s)); 
 }
 
 }
